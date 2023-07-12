737408027a7857e7527bd077bcceee72591c02fa
tambongslade <tambongslade17@gmail.com>
2023-07-12 04:29:47 +0100

    day 2

---
+++ day2.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+rage=int(age)
+leftyears=90-rage
+monthsleft=12*leftyears
+weeksleft=leftyears*52
+days=leftyears*365
+print(f"You have {days} days, {weeksleft} weeks, and{monthsleft} months left.")
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+newheight=float(height)
+newWeight=int(weight)
+bmi= newWeight/(newheight*newheight)
+print(int(bmi))
+
+
+
