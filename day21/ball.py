f70e5de3af983d3739236116b9af46a9a1edaba1
tambongslade <tambongslade17@gmail.com>
2023-08-30 16:51:53 +0100

    pong game finish

--- day21/ball.py
+++ day21/ball.py
@@ -7,9 +7,11 @@ class Ball(Turtle):
         self.penup()
         self.x_move= 10
         self.y_move=10
+        self.Move_speed=0.1
     
     def bouncey(self):
         self.y_move *=-1
+        self.Move_speed*=0.9
     def bouncex(self):
         self.x_move *=-1
     def move(self):
@@ -17,3 +19,8 @@ class Ball(Turtle):
         new_x=self.xcor()+ self.x_move
         new_y=self.ycor()+ self.y_move
         self.goto(new_x,new_y)
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.Move_speed=0.1
+        self.bouncex()
--- day21/main.py
+++ day21/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen,Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 screen=Screen()
 screen.bgcolor("black")
@@ -10,6 +11,7 @@ screen.tracer(0)
 r_paddle=Paddle((350,0))
 l_paddle=Paddle((-350,0))
 ball=Ball()
+score=Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up,"Up")
@@ -19,13 +21,27 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on=True
 while game_is_on==True:
-    time.sleep(0.1)
+    time.sleep(ball.Move_speed)
     screen.update()
     ball.move()
+    #detection ball with wall
     if ball.ycor()>280 or ball.ycor()<-280:
         ball.bouncey()
-    
-    if ball.distance(r_paddle)<50 and ball.xcor()>30 or ball.distance(l_paddle)<50 and ball.xcor()>-30:
+    #detection of ball with paddle
+    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()<-320:
         ball.bouncex()
+
+    #detect R padle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+    
+        score.l_point()
+      
+   
+     #detect l padle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+    
+        score.r_point()
 
 screen.exitonclick()
