aa12e9e5d8cfd55ef8b28c4f73673692c89eaf25
tambongslade <tambongslade17@gmail.com>
2023-09-09 14:41:06 +0100

    password manager

--- day29/password-manager-start/main.py
+++ day29/password-manager-start/main.py
@@ -7,7 +7,7 @@ import  json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generatePassword():
     import random
-
+    pass_input.delete(0,END)
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v',
                'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
@@ -16,10 +16,7 @@ def generatePassword():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = int(input("How many letters would you like in your password?\n"))
 
-    nr_symbols = int(input(f"How many symbols would you like?\n"))
-    nr_numbers = int(input(f"How many numbers would you like?\n"))
     password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
     password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
     password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
