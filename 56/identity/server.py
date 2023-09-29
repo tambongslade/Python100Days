c6c0c480b80ba7754880f658148c6b5130de7c06
tambongslade <tambongslade17@gmail.com>
2023-09-29 08:39:18 +0100

    lol

---
+++ 56/identity/server.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def Home():
+    return render_template('index.html')
+
+if app =='__main__':
+    app.run(debug=True)
